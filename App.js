ae0d253eef54cc4919189c5b6d1b12ea816b59d9
PODODANG <adadhaha96@gmail.com>
2026-04-23 00:18:26 +0900

    App.js 업데이트

--- App.js
+++ App.js
@@ -1,1 +1,17 @@
-import
+import logo from './logo.svg';
+import '/App.css';
+
+function App() {
+	return (
+    	<div className="App">
+        	<header className="App-header">
+            	<img src={logo} className="App-logo" alt="logo" />
+                <p>
+                	hellow ! React !
+                </p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
