28b2a8190a1562921ebd0b51863e55fb9a0cff47
PODODANG <adadhaha96@gmail.com>
2026-04-23 00:21:46 +0900

    App.test.js 업데이트

--- App.test.js
+++ App.test.js
@@ -1,1 +1,8 @@
 import { render, screen } from '@testing-library/react';
+import App from "./App';
+
+test('renders learn react link', () => {
+	render (<App />);
+    const linkElement = screen.getByTest(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+});
